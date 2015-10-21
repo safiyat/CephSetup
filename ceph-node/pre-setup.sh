abebf7a31d02fe1649bbf4e3c9ff63122f86e36f
Md Safiyat Reza <md.reza@snapdeal.com>
2015-10-21 15:40:52 +0530

    Finished grep functionality.

--- ceph-node/pre-setup.sh
+++ ceph-node/pre-setup.sh
@@ -2,6 +2,8 @@
 sudo chmod +w /etc/sudoers
 if [[ `sudo grep -c "^ *ALL *ALL *= *(ALL) *NOPASSWD *: *ALL" /etc/sudoers` -eq 0 ]]
 then
-    echo "ALL ALL=(ALL) NOPASSWD:ALL" | sudo tee --append /etc/sudoers
+    echo "ALL ALL=(ALL) NOPASSWD:ALL" | sudo tee --append /etc/sudoers > /dev/null
+    echo "Added password-less SUDO access for all users."
 fi
 sudo chmod -w /etc/sudoers
+
