be20687b17b89bd48e99215ec478ed8cd71e6783
Safiyat Reza <md.reza@snapdeal.com>
2015-10-28 16:57:54 +0530

    Work in progress. The generated key is not queried.

--- ceph-deploy/passwordless-ssh.sh
+++ ceph-deploy/passwordless-ssh.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
 
+SSH_KEY_FILE=~/.ssh/ceph_ssh_key
+
+REMOTE_USER=safiyat
+REMOTE_HOST=10.0.0.66
+
+#Generate the public/private rsa key pair.
+ssh-keygen -q -t rsa -P "" -f $SSH_KEY_FILE
+
+ssh $REMOTE_USER@$REMOTE_HOST mkdir -p .ssh
+
+cat $SSH_KEY_FILE.pub | ssh  $REMOTE_USER@$REMOTE_HOST 'cat >> .ssh/authorized_keys'
